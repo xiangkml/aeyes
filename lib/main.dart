bbbab1e9c03026c95429513801431d260aca362b
kongjy <kongjiyu0198@gmail.com>
2026-03-06 17:29:01 +0800

    Move API key to .env and add to .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const AEyesApp());
 }
